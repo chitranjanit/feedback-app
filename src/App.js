a833b04d7a54a62d43bb02cb4d3639f5ac1f1c7f
chitranjan <chitranjan.mukhiya@exaltaretech.com>
2022-08-09 09:59:53 +0530

    feedback app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import { useState } from "react";
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import { v4 as uuidv4 } from "uuid";
 import Header from "./components/Header";
 import FeedbackList from "./components/FeedbackList";
 import FeedbackData from "./components/FeedbackData";
 import FeedbackStats from "./components/FeedbackStats";
 import FeedbackForm from "./components/FeedbackForm";
+import AboutPage from "./pages/AboutPage";
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
@@ -25,14 +27,14 @@ function App() {
   };
 
   return (
-    <>
+    <Router>
       <Header />
       <div className="container">
         <FeedbackForm handleAdd={addFeedback} />
         <FeedbackStats feedback={feedback} />
         <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>
-    </>
+    </Router>
   );
 }
 
